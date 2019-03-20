6349f8554da5c0fad9beae4957928101999fb38a
kamil <kamil_grabowski@vp.pl>
2019-03-20 21:56:57 +0100

    add AppBar

--- src/component/App.js
+++ src/component/App.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import TextField from "@material-ui/core/TextField";
 import withStyles from "@material-ui/core/es/styles/withStyles";
+import AppBarCalculator from './AppBarCalculator';
 
 const styles = theme => ({
     textField: {
@@ -13,56 +14,56 @@ const styles = theme => ({
 });
 
 
-
 class App extends React.Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            timeToRefresh:'',
+            timeToRefresh: '',
         }
     }
 
 
-
     handelAlert = () => {
-        setTimeout(()=>{
-            alert("Wpisałeś czas: " + this.state.timeToRefresh);},
+        setTimeout(() => {
+                alert("Wpisałeś czas: " + this.state.timeToRefresh);
+            },
             this.state.timeToRefresh)
-        };
+    };
 
     handleTimeToRefresh = timeToRefresh => event => {
-      this.setState({
-          [timeToRefresh]: event.target.value
-      })
+        this.setState({
+            [timeToRefresh]: event.target.value
+        })
     };
 
 
-
-
-  render() {
-      const { classes } = this.props;
-    return (
-      <div >
-          <TextField
-              id="timeToRefreshId"
-              label="Write time to refresh"
-              className={classes.textField}
-              value={this.state.timeToRefresh}
-              onChange={this.handleTimeToRefresh('timeToRefresh')}
-              margin="normal"
-          />
-        <Button
-            variant="contained"
-            color="primary"
-            onClick={this.handelAlert}
-        >
-          Hello Kamil
-        </Button>
-      </div>
-    );
-  }
+    render() {
+        const {classes} = this.props;
+        return (
+            <div>
+                <AppBarCalculator/>
+                <div>
+                    <TextField
+                        id="timeToRefreshId"
+                        label="Write time to refresh"
+                        className={classes.textField}
+                        value={this.state.timeToRefresh}
+                        onChange={this.handleTimeToRefresh('timeToRefresh')}
+                        margin="normal"
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handelAlert}
+                    >
+                        Hello Kamil
+                    </Button>
+                </div>
+            </div>
+        );
+    }
 }
 
 App.propTypes = {
