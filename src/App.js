9404f55446f91fd28e2c0a10209e06cafffe4b0e
kamil <kamil_grabowski@vp.pl>
2019-03-20 19:07:01 +0100

    TextField(how many time add to setTimeout()) & Button(alert)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import TextField from "@material-ui/core/TextField";
+import withStyles from "@material-ui/core/es/styles/withStyles";
+
+const styles = theme => ({
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 200,
+    },
+});
+
+
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            timeToRefresh:'',
+        }
+    }
+
+
+
+    handelAlert = () => {
+        setTimeout(()=>{
+            alert("Siemanderko");},
+            this.state.timeToRefresh)
+        };
+
+    handleTimeToRefresh = timeToRefresh => event => {
+      this.setState({
+          [timeToRefresh]: event.target.value
+      })
+    };
+
+
+
+
   render() {
+      const { classes } = this.props;
     return (
       <div >
-        <Button variant="contained" color="primary">
-          Hello World
+          <TextField
+              id="timeToRefreshId"
+              label="Write time to refresh"
+              className={classes.textField}
+              value={this.state.timeToRefresh}
+              onChange={this.handleTimeToRefresh('timeToRefresh')}
+              margin="normal"
+          />
+        <Button
+            variant="contained"
+            color="primary"
+            onClick={this.handelAlert}
+        >
+          Hello Kamil
         </Button>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
