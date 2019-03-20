063d8b5aaaa9eeb906762e47ada7394cab38756d
kamil <kamil_grabowski@vp.pl>
2019-03-20 22:09:58 +0100

    orange color in AppBar

--- src/component/AppBarCalculator.js
+++ src/component/AppBarCalculator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -12,21 +12,28 @@ const styles = {
     },
     grow: {
         flexGrow: 1,
+        textAlign: 'center',
     },
     menuButton: {
         marginLeft: -12,
         marginRight: 20,
     },
+    navBar: {
+        backgroundColor: '#FF8C00',
+    },
 };
 
 function AppBarCalculator(props) {
-    const { classes } = props;
+    const {classes} = props;
     return (
         <div className={classes.root}>
             <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" color="inherit" className={classes.grow}>
-                        News
+                <Toolbar className={classes.navBar}>
+                    <Typography
+                        variant="h6"
+                        color="inherit"
+                        className={classes.grow}>
+                        CALCULATOR
                     </Typography>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
