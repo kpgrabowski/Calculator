b15e6167e49b7e73144a74aa5c75b493e47451cb
kamil <kamil_grabowski@vp.pl>
2019-03-20 18:28:55 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Siemandero321
+            Siemandero3212
           </a>
         </header>
       </div>
