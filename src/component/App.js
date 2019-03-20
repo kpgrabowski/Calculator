66bb66c1f1684317e53223027686f83054a0b564
kamil <kamil_grabowski@vp.pl>
2019-03-20 19:38:09 +0100

    remove unnecessary fields

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import TextField from "@material-ui/core/TextField";
